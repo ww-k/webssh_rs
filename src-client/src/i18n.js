e1f33dbc1eb6d05ea961f71279e40636575a03c0
kevinwon <kevinwon1985@qq.com>
2026-06-26 16:46:15 +0800

    fix tsconfig TypeScript 7.0 error

--- src-client/src/i18n.js
+++ src-client/src/i18n.js
@@ -1,8 +1,9 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
-import translation from "public/locales/zh/translation.json";
 import { initReactI18next } from "react-i18next";
+
+import translation from "../public/locales/zh/translation.json";
 
 i18n
     // load translation using http -> see /public/locales
